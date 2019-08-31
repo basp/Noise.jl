d2ee8f7b320e0a8cd302e031235d2d5c0614c888
Bas Pennings <baspster@gmail.com>
2019-08-31 11:59:59 +0100

    Starting on Perlin

--- src/Noise.jl
+++ src/Noise.jl
@@ -2,7 +2,7 @@ module Noise
 
 using LinearAlgebra
 
-export Worley
+export Worley, Perlin
 
 struct Worley 
     fs
@@ -11,9 +11,25 @@ end
 
 Worley(n) = Worley(2, n)
 
-function (G::Worley)(n, x)
-    Δ = map(f -> norm(x - f), eachcol(G.fs)) |> sort
+function (G::Worley)(n, p)
+    Δ = map(f -> norm(p - f), eachcol(G.fs)) |> sort
     return Δ[n]
 end
+
+struct Perlin
+    field
+end
+
+Perlin(xs, ys) = [rand(2) for _ in 1:xs, _ in 1:ys] |> Perlin
+
+function (G::Perlin)(p)
+    M = size(G.field) |> collect |> Diagonal
+    p = M * p
+    A = floor.(p)
+    B = A .+ 1
+end
+
+# 1,1 => [(1,1), (1, 2), (2, 1), (2, 2)]
+# 1,1,1 => [(1,1,1), (1,1,2), (1,2,1), (1,2,2), (2,1,1), (2, 1, 2), (2, 2, 2)]
 
 end # module
