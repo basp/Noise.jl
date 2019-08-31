2607d17206f3e489b1600998912a17d3864d8d37
Bas Pennings <baspster@gmail.com>
2019-08-31 09:03:31 +0100

    Worley

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Noise.jl
-Noise
+## Worley
+```
+julia> G = Worley(1024)
+Worley([0.0195399 0.48826 … 0.801658 0.660078])
+
+julia> F1(x, y) = G(1, [x, y])
+F1 (generic function with 1 method)
+
+julia> xaxis = range(0, 1, length=640)
+0.0:0.001564945226917058:1.0
+
+julia> yaxis = range(0, 1, length=480)
+0.0:0.0020876826722338203:1.0
+
+julia> A = [F1(x, y) for x in xaxis, y in yaxis]
+640×480 Array{Float64,2}:
+...
+
+julia> map(Gray, A / maximum(A)) |> plot
+```
--- src/Noise.jl
+++ src/Noise.jl
@@ -4,16 +4,28 @@ using LinearAlgebra
 using Images
 using Plots
 
-export worley
+export Worley
 
-function worley(W, H, n)
-    M = [ W 0; 0 H]
-    features = [rand(2) for _ in 1:n] |> fs -> map(f -> ceil.(Int, M * f), fs)
-    F1(x) = minimum(map(f -> norm(x - f), features))
-    Δ = [F1([x,y]) for y in 1:H, x in 1:W]
-    Δmax = maximum(Δ)
-    Δnorm = map(δ -> δ / Δmax, Δ)
-    map(Gray, Δnorm)
+struct Worley 
+    fs
+    Worley(d, n) = new(rand(d, n))
 end
+
+Worley(n) = Worley(2, n)
+
+function (G::Worley)(n, x)
+    Δ = map(f -> norm(x - f), eachcol(G.fs)) |> sort
+    return Δ[n]
+end
+
+# function _worley(W, H, n)
+#     M = [ W 0; 0 H]
+#     features = [rand(2) for _ in 1:n] |> fs -> map(f -> ceil.(Int, M * f), fs)
+#     F1(x) = minimum(map(f -> norm(x - f), features))
+#     Δ = [F1([x,y]) for y in 1:H, x in 1:W]
+#     Δmax = maximum(Δ)
+#     Δnorm = Δ / Δmax
+#     map(Gray, Δnorm)
+# end
 
 end # module
